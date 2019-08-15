08a3da41c9b8a35af17e317b45f1d92806e21814
Xiaomi <liuchaowei0310@outlook.com>
2019-08-15 19:30:09 +0800

    菜单

--- Xiaomi/App/models.py
+++ Xiaomi/App/models.py
@@ -5,18 +5,27 @@ class Index(models.Model):
     id = models.AutoField(primary_key=True)
     menu = models.CharField(max_length=40)
     mid = models.IntegerField()
-    pay_menu = models.CharField(max_length=20,default=menu)
-    big_image = models.ImageField()
-    image = models.ImageField()
+    pay_menu = models.CharField(max_length=20)
+    image = models.CharField(max_length=100)
     big_title = models.CharField(max_length=10)
     pay_title = models.CharField(max_length=20)
-    select = models.CharField(max_length=20)
+    select = models.CharField(max_length=20,null=True)
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=100)
     price = models.IntegerField()
-    plate_id = models.IntegerField()
+    plate_id = models.IntegerField(null=True)
     product_description = models.CharField(max_length=100)
-
+    big_image = models.CharField(max_length=100,null=True)
 
     class Meta:
         db_table = 'Xiaomi_index'
+
+class Paymenu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+    content = models.CharField(max_length=50)
+    pid = models.ImageField()
+
+    class Meta:
+        db_table = 'xiaomi_PayTitle'
--- Xiaomi/App/views.py
+++ Xiaomi/App/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from App.models import Index
+from App.models import Index, Paymenu
+
 
 # Create your views here.
 
 
 def index(request):
     menus = Index.objects.all()
-    return render(request, "index.html", context={"menus": menus})
-	
+    title = Paymenu.objects.all()
+
+    return render(request, "index.html", context={"menus": menus, 'title': title})
