259bfd58ef60903dac56e4ebfc1dfd4aa67cdfac
Xiaomi <liuchaowei0310@outlook.com>
2019-08-20 20:27:11 +0800

    数据页面跳转

--- Xiaomi/App/views.py
+++ Xiaomi/App/views.py
@@ -1,5 +1,8 @@
+import re
+
 from django.shortcuts import render
 from App.models import Index, Paymenu, List, Detail
+
 
 # Create your views here.
 
@@ -10,18 +13,24 @@ def index(request):
 
     return render(request, "index.html", context={"menus": menus, 'title': title})
 
+
 def list(request):
     title = Paymenu.objects.all()
     list = List.objects.all()
-    return render(request, 'list.html',{'titles':title,'lists':list})
+    return render(request, 'list.html', {'titles': title, 'lists': list})
+
 
 def detail(request):
-    detail = Detail.objects.all()
-    list = List.objects.all()
-    return render(request,'details.html',{'details':detail,'lists':list})
+    detail = Index.objects.all()
+    image = Detail.objects.all()
+    id = request.GET.get('id')
+    parameter = int(id)
+    return render(request, 'details.html', context={'details': detail, 'parameter_id': parameter,'image':image})
+
 
 def shopping_cart(request):
-    return render(request,'shopping_cart.html')
+    return render(request, 'shopping_cart.html')
+
 
 def settlement(request):
-    return render(request,'settlement.html')
+    return render(request, 'settlement.html')
